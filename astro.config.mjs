f461b88dbbfec44df57d0d0edcc61c4ab7bb0833
snowsphere <info@snowsphere.net>
2023-04-30 11:43:16 +0900

    feat: ヘッダーのデザイン変更とメタデータの整理

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://e2014.prismistim.com',
   integrations: [tailwind()]
-});
+})
